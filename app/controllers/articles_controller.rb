9fe587ecdd838b5e7ca4de01aa471339b3e8ebb5
Plínio Larrubia <plinio2xd@gmail.com>
2021-10-09 15:16:56 -0300

    Chapter 10: Insert basic security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "user", password: "123456", except: %i[index show]
+
   def index
     @articles = Article.all
   end
